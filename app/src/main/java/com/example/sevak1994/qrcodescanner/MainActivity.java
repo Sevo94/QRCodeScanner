97897198ede7eb1cc1a16b5b411941487f15ccbc
sevag mardirossian <sevakprogrammer94@gmail.com>
2017-09-08 16:23:57 -0700

    "QR code scanner added"

--- app/src/main/java/com/example/sevak1994/qrcodescanner/MainActivity.java
+++ app/src/main/java/com/example/sevak1994/qrcodescanner/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sevak1994.qrcodescanner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -7,6 +8,10 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,24 +26,34 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    fragmentManager.startHomeFragment(fragmentActivity);
+                    if (!item.isChecked()) {
+                        mTextMessage.setText(R.string.title_home);
+                        fragmentManager.startHomeFragment(fragmentActivity);
+                    }
                     return true;
                 case R.id.navigation_contacts:
-                    mTextMessage.setText(R.string.title_contacts);
-                    fragmentManager.startContactsFragment(fragmentActivity);
+                    if (!item.isChecked()) {
+                        mTextMessage.setText(R.string.title_contacts);
+                        fragmentManager.startContactsFragment(fragmentActivity);
+                    }
                     return true;
                 case R.id.navigation_qr_scanner:
-                    mTextMessage.setText(R.string.title_qr_scanner);
-                    fragmentManager.startQRScannerFragment(fragmentActivity);
+                    if (!item.isChecked()) {
+                        mTextMessage.setText(R.string.title_qr_scanner);
+                        fragmentManager.startQRScannerFragment(fragmentActivity);
+                    }
                     return true;
                 case R.id.navigation_qr_code:
-                    mTextMessage.setText(R.string.title_qr_code);
-                    fragmentManager.startQRCodeFragment(fragmentActivity);
+                    if (!item.isChecked()) {
+                        mTextMessage.setText(R.string.title_qr_code);
+                        fragmentManager.startQRCodeFragment(fragmentActivity);
+                    }
                     return true;
                 case R.id.navigation_settings:
-                    mTextMessage.setText(R.string.title_settings);
-                    fragmentManager.startSettigsFragment(fragmentActivity);
+                    if (!item.isChecked()) {
+                        mTextMessage.setText(R.string.title_settings);
+                        fragmentManager.startSettigsFragment(fragmentActivity);
+                    }
                     return true;
             }
             return false;
@@ -56,5 +71,20 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         BottomNavigationViewHelper.disableShiftMode(navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Scanned " + result.getContents(), Toast.LENGTH_SHORT).show();
+            }
+
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
--- app/src/main/java/com/example/sevak1994/qrcodescanner/fragments/QRScannerFragment.java
+++ app/src/main/java/com/example/sevak1994/qrcodescanner/fragments/QRScannerFragment.java
@@ -1,13 +1,18 @@
 package com.example.sevak1994.qrcodescanner.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.sevak1994.qrcodescanner.R;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 /**
  * Created by Admin on 9/7/2017.
@@ -15,7 +20,7 @@ import com.example.sevak1994.qrcodescanner.R;
 
 public class QRScannerFragment extends Fragment {
 
-    private View fragmentRootView;
+    private FragmentActivity activity;
 
     public QRScannerFragment() {
     }
@@ -23,13 +28,39 @@ public class QRScannerFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        fragmentRootView = inflater.inflate(R.layout.fragment_contacts, container, false);
-
-        return fragmentRootView;
+        return inflater.inflate(R.layout.fragment_contacts, container, false);
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        activity = getActivity();
+        initQRScanner();
+    }
+
+    public void initQRScanner() {
+        IntentIntegrator intentIntegrator = new IntentIntegrator(activity);
+        intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+        intentIntegrator.setPrompt("Scan");
+        intentIntegrator.setCameraId(0);
+        intentIntegrator.setBeepEnabled(false);
+        intentIntegrator.setBarcodeImageEnabled(false);
+        IntentIntegrator.forSupportFragment(this).initiateScan();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(activity, "Cancelled", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(activity, "Scanned " + result.getContents(), Toast.LENGTH_SHORT).show();
+            }
+
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
