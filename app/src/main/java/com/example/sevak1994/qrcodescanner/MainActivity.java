8b0291d357abebc19d00e534306cd9f083dd2caf
sevag mardirossian <sevakprogrammer94@gmail.com>
2017-09-07 18:26:51 -0700

    "disable animation on bottom navView"

--- app/src/main/java/com/example/sevak1994/qrcodescanner/MainActivity.java
+++ app/src/main/java/com/example/sevak1994/qrcodescanner/MainActivity.java
@@ -41,8 +41,5 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        Log.d("", "");
     }
-
 }
---
+++ app/src/main/java/com/example/sevak1994/qrcodescanner/fragments/QRCodeFragment.java
@@ -0,0 +1,35 @@
+package com.example.sevak1994.qrcodescanner.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sevak1994.qrcodescanner.R;
+
+/**
+ * Created by Admin on 9/7/2017.
+ */
+
+public class QRCodeFragment extends Fragment {
+
+    private View fragmentRootView;
+
+    public QRCodeFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        fragmentRootView = inflater.inflate(R.layout.fragment_contacts, container, false);
+
+        return fragmentRootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
