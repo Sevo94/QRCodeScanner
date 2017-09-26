f78d4d3499e0b3f158308950508b94e50624f4a8
sevag mardirossian <sevakprogrammer94@gmail.com>
2017-09-25 17:44:28 -0700

    ""

--- app/src/main/java/com/example/sevak1994/qrcodescanner/fragments/HomeFragment.java
+++ app/src/main/java/com/example/sevak1994/qrcodescanner/fragments/HomeFragment.java
@@ -26,17 +26,8 @@ import com.example.sevak1994.qrcodescanner.helper.BitmapUtils;
 public class HomeFragment extends Fragment {
 
     private View fragmentRootView;
-    private HomeActivity activity;
-
-    private LinearLayout mobileLayout;
-    private LinearLayout emailLayout;
-    private LinearLayout addressLayout;
 
     private ImageView blurProfilePicture;
-    private Bitmap bitmap;
-
-    private ImageView miniProIV;
-    private ImageView websiteIV;
 
     public HomeFragment() {
     }
@@ -52,7 +43,7 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        activity = (HomeActivity) getActivity();
+        HomeActivity activity = (HomeActivity) getActivity();
 
         activity.inNormalMode(false);
         activity.setToolbarTitle(getResources().getString(R.string.title_home));
@@ -83,20 +74,9 @@ public class HomeFragment extends Fragment {
                 .dontAnimate()
                 .placeholder(R.drawable.default_photo)
                 .into(simpleTarget);
-
-//        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.profile);
-//        blurProfilePicture.setImageBitmap(BitmapUtils.fastblur(bitmap, 0.04f, 5));
     }
 
     private void initFragmentUi() {
-//        mobileLayout = fragmentRootView.findViewById(R.id.mobile);
-//        emailLayout = fragmentRootView.findViewById(R.id.email);
-//        addressLayout = fragmentRootView.findViewById(R.id.address);
         blurProfilePicture = fragmentRootView.findViewById(R.id.blur_pro_pic);
-        miniProIV = fragmentRootView.findViewById(R.id.mini_pro_iv);
-        websiteIV = fragmentRootView.findViewById(R.id.website_iv);
-
-        miniProIV.setColorFilter(getResources().getColor(R.color.white));
-        websiteIV.setColorFilter(getResources().getColor(R.color.white));
     }
 }
